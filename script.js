b15c9c1be6d1da63a67ffa67f7672c412da49b94
rasikasrimal <rasikacooldj618@gmail.com>
2025-08-08 11:06:54 +0530

    update

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const d1 = {
   'MzY1NDM2Njk2': 'eyJleHBpcmF0aW9uIjoiMjAyNS0wOC0zMVQxOTo0MTowMCswNTozMCJ9',
   'NzUzNTI0ODM5': 'eyJleHBpcmF0aW9uIjoiMjAyNS0wOS0wN1QxOTo0MTowMCswNTozMCJ9',
   'OTIzODI0NzQx': 'eyJleHBpcmF0aW9uIjoiMjAyNS0wNy0zMFQwMDowMDowMCswNTozMCJ9',
-  'NTE0NTY0NDc4Ng==': 'eyJleHBpcmF0aW9uIjoiMjAyNS0wOS0wN1QwMDowMDowMCswNTozMCJ9',
+  'NTE0NTY0NDc4Ng==': 'eyJleHBpcmF0aW9uIjoiMjAyNS0wOC0wN1QwMDowMDowMCswNTozMCJ9',
   'MTkxODMwMTkxNA==': 'eyJleHBpcmF0aW9uIjoiMjAyNS0wOC0wM1QwMDowMDowMCswNTozMCJ9',
   'Mjg4MjQyNTI1': 'eyJleHBpcmF0aW9uIjoiMjAyNi0wNS0wMVQwMDowMDowMCswNTozMCJ9',
   'MjEzODQxODg2OQ==': 'eyJleHBpcmF0aW9uIjoiMjAyNi0wNS0wMVQwMDowMDowMCswNTozMCJ9',
@@ -30,6 +30,7 @@ const d1 = {
   'MzI4MzEwODcxMg==': 'eyJleHBpcmF0aW9uIjoiMjAyOC0wNC0xNFQxMjowMDowMCswNTozMCJ9',
   'MTE1MzE4NjE4MA==': 'eyJleHBpcmF0aW9uIjoiMjAyNS0wOC0xNVQwMDowMDowMCswNTozMCJ9'
 };
+
 
 let skipAnimations = true;
 
